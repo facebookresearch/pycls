98ae8fd4a65fd7e7502c0042d4eee91d5b371ac7
Ilija Radosavovic <ilijar@fb.com>
2019-06-17 12:10:17 -0700

    Refactor lr policy function retrieval
    fbshipit-source-id: 9716ef01c80e0ffe3d632b679cc234a85d343917

--- pycls/utils/lr_policy.py
+++ pycls/utils/lr_policy.py
@@ -7,38 +7,36 @@ import numpy as np
 from pycls.core.config import cfg
 
 
-def lr_func_steps(cur_epoch):
+def lr_fun_steps(cur_epoch):
     """Steps schedule (cfg.OPTIM.LR_POLICY = 'steps')."""
     ind = [i for i, s in enumerate(cfg.OPTIM.STEPS) if cur_epoch >= s][-1]
     return cfg.OPTIM.BASE_LR * (cfg.OPTIM.LR_MULT ** ind)
 
 
-def lr_func_exp(cur_epoch):
+def lr_fun_exp(cur_epoch):
     """Exponential schedule (cfg.OPTIM.LR_POLICY = 'exp')."""
     return cfg.OPTIM.BASE_LR * (cfg.OPTIM.GAMMA ** cur_epoch)
 
 
-def lr_func_cos(cur_epoch):
+def lr_fun_cos(cur_epoch):
     """Cosine schedule (cfg.OPTIM.LR_POLICY = 'cos')."""
     base_lr, max_epoch = cfg.OPTIM.BASE_LR, cfg.OPTIM.MAX_EPOCH
     return 0.5 * base_lr * (1.0 + np.cos(np.pi * cur_epoch / max_epoch))
 
 
-def get_lr_func():
+def get_lr_fun():
     """Retrieves the specified lr policy function"""
-    policy = 'lr_func_' + cfg.OPTIM.LR_POLICY
-    if policy not in globals():
-        raise NotImplementedError(
-            'Unknown LR policy: {}'.format(cfg.OPTIM.LR_POLICY)
-        )
-    return globals()[policy]
+    lr_fun = 'lr_fun_' + cfg.OPTIM.LR_POLICY
+    if lr_fun not in globals():
+        raise NotImplementedError('Unknown LR policy:' +  cfg.OPTIM.LR_POLICY)
+    return globals()[lr_fun]
 
 
 def get_epoch_lr(cur_epoch):
     """Retrieves the lr for the given epoch according to the policy."""
-    lr = get_lr_func()(cur_epoch)
+    lr = get_lr_fun()(cur_epoch)
+    # Linear warmup
     if cur_epoch < cfg.OPTIM.WARMUP_EPOCHS:
-        # Linear warmup
         alpha = cur_epoch / cfg.OPTIM.WARMUP_EPOCHS
         warmup_factor = cfg.OPTIM.WARMUP_FACTOR * (1.0 - alpha) + alpha
         lr *= warmup_factor
