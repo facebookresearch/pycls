dbb9123992866a26564cdd7428db8833abb942ad
Raj Prateek Kosaraju <kosaraju@fb.com>
2020-05-21 14:18:36 -0700

    Update test_complexity to use full paths
    Summary: Updating dev/test_complexity.py to use full paths to configs while searching for them (in `dump_complexity()`) and while loading them (in `test_complexity()`)
    
    Reviewed By: pdollar
    
    Differential Revision: D21682999
    
    fbshipit-source-id: a7574e4e530c0387960e7378812c25b0c3ba6b5f

--- dev/test_complexity.py
+++ dev/test_complexity.py
@@ -18,19 +18,26 @@ from parameterized import parameterized
 from pycls.core.config import cfg
 
 
-# Location to store complexity of all models in configs/
-_COMPLEXITY_FILE = "dev/complexity.json"
+# Location of pycls directory
+_PYCLS_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+
+# Location of all configs
+_CONFIGS_DIR = os.path.join(_PYCLS_DIR, "configs")
+
+# Location to store complexity of all models in configs
+_COMPLEXITY_FILE = os.path.join(_PYCLS_DIR, "dev/complexity.json")
 
 
 def dump_complexity():
     """Measure the complexity of every model in the configs/ directory."""
     complexity = {"date-created": str(datetime.datetime.now())}
-    cfg_files = [os.path.join(r, f) for r, _, fs in os.walk("configs/") for f in fs]
+    cfg_files = [os.path.join(r, f) for r, _, fs in os.walk(_CONFIGS_DIR) for f in fs]
     cfg_files = sorted(f for f in cfg_files if ".yaml" in f)
     for cfg_file in cfg_files:
         cfg_init = cfg.clone()
         cfg.merge_from_file(cfg_file)
-        complexity[cfg_file] = net.complexity(builders.get_model())
+        cfg_file_rel = os.path.relpath(cfg_file, _PYCLS_DIR)
+        complexity[cfg_file_rel] = net.complexity(builders.get_model())
         cfg.merge_from_other_cfg(cfg_init)
     with open(_COMPLEXITY_FILE, "w") as file:
         json.dump(complexity, file, sort_keys=True, indent=4)
@@ -48,9 +55,10 @@ class TestModelComplexity(unittest.TestCase):
     """Test the complexity of every model in the configs/ directory."""
 
     @parameterized.expand(load_complexity())
-    def test_complexity(self, cfg_file, cx_expected):
+    def test_complexity(self, cfg_file_rel, cx_expected):
         """Test complexity of a single model with the specified config."""
         cfg_init = cfg.clone()
+        cfg_file = os.path.join(_PYCLS_DIR, cfg_file_rel)
         cfg.merge_from_file(cfg_file)
         cx = net.complexity(builders.get_model())
         cfg.merge_from_other_cfg(cfg_init)
