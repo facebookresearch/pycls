00bf93ee11536b8a7e40ac5057cdc56d356af8f6
Piotr Dollar <pdollar@gmail.com>
2020-05-25 07:56:39 -0700

    Add timing of data loader and new defaults (#75)
    Summary:
    data loading can introduce a large overhead, especially when
    training small models. This commit adds code to time data loading and
    also changes the default number of CPU workers per GPU to 8, which helps
    training of small models when CPUs largely outnumber GPUs, as is common.
    
    -benchmark.py:
    --compute_time_eval()/train() moved here form net.py
    --compute_time_loader() new func to time data loader
    --compute_time_full() new func to time model+loader for iter+epochs
    -config.py: DATA_LOADER.NUM_WORKERS set to 8
    -net.py: moved timing function to benchmark.py
    -trainer.py: uses benchmark.compute_time_full()
    -time_net.py: updated block doc and prints
    
    Pull Request resolved: https://github.com/facebookresearch/pycls/pull/75
    
    Reviewed By: rajprateek
    
    Differential Revision: D21712741
    
    Pulled By: pdollar
    
    fbshipit-source-id: 13eb1027f5a6873c9ad68a9f880112e895fdda06

--- tools/time_net.py
+++ tools/time_net.py
@@ -5,7 +5,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-"""Compute precise time for a model on 1 GPU."""
+"""Compute model and loader timings."""
 
 import pycls.core.config as config
 import pycls.core.distributed as dist
@@ -14,7 +14,7 @@ from pycls.core.config import cfg
 
 
 def main():
-    config.load_cfg_fom_args("Compute precise time for a model on 1 GPU.")
+    config.load_cfg_fom_args("Compute model and loader timings.")
     config.assert_and_infer_cfg()
     cfg.freeze()
     dist.multi_proc_run(num_proc=cfg.NUM_GPUS, fun=trainer.time_model)
