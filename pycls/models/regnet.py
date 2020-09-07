603e55a7e4185bbf20acd2c948eae4c837b1a115
Piotr Dollar <pdollar@gmail.com>
2020-09-07 13:34:45 -0700

    model refactor

--- pycls/models/regnet.py
+++ pycls/models/regnet.py
@@ -8,82 +8,62 @@
 """RegNet models."""
 
 import numpy as np
+import pycls.models.blocks as bk
 from pycls.core.config import cfg
 from pycls.models.anynet import AnyNet
 
 
-def quantize_float(f, q):
-    """Converts a float to closest non-zero int divisible by q."""
-    return int(round(f / q) * q)
-
-
-def adjust_ws_gs_comp(ws, bms, gs):
-    """Adjusts the compatibility of widths and groups."""
-    ws_bot = [int(w * b) for w, b in zip(ws, bms)]
-    gs = [min(g, w_bot) for g, w_bot in zip(gs, ws_bot)]
-    ws_bot = [quantize_float(w_bot, g) for w_bot, g in zip(ws_bot, gs)]
-    ws = [int(w_bot / b) for w_bot, b in zip(ws_bot, bms)]
-    return ws, gs
-
-
-def get_stages_from_blocks(ws, rs):
-    """Gets ws/ds of network at each stage from per block values."""
-    ts_temp = zip(ws + [0], [0] + ws, rs + [0], [0] + rs)
-    ts = [w != wp or r != rp for w, wp, r, rp in ts_temp]
-    s_ws = [w for w, t in zip(ws, ts[:-1]) if t]
-    s_ds = np.diff([d for d, t in zip(range(len(ts)), ts) if t]).tolist()
-    return s_ws, s_ds
-
-
 def generate_regnet(w_a, w_0, w_m, d, q=8):
-    """Generates per block ws from RegNet parameters."""
+    """Generates per stage widths and depths from RegNet parameters."""
     assert w_a >= 0 and w_0 > 0 and w_m > 1 and w_0 % q == 0
+    # Generate continuous per-block ws
     ws_cont = np.arange(d) * w_a + w_0
+    # Generate quantized per-block ws
     ks = np.round(np.log(ws_cont / w_0) / np.log(w_m))
-    ws = w_0 * np.power(w_m, ks)
-    ws = np.round(np.divide(ws, q)) * q
-    num_stages, max_stage = len(np.unique(ws)), ks.max() + 1
-    ws, ws_cont = ws.astype(int).tolist(), ws_cont.tolist()
-    return ws, num_stages, max_stage, ws_cont
+    ws_all = w_0 * np.power(w_m, ks)
+    ws_all = np.round(np.divide(ws_all, q)).astype(int) * q
+    # Generate per stage ws and ds (assumes ws_all are sorted)
+    ws, ds = np.unique(ws_all, return_counts=True)
+    # Compute number of actual stages and total possible stages
+    num_stages, total_stages = len(ws), ks.max() + 1
+    # Convert numpy arrays to lists and return
+    ws, ds, ws_all, ws_cont = (x.tolist() for x in (ws, ds, ws_all, ws_cont))
+    return ws, ds, num_stages, total_stages, ws_all, ws_cont
 
 
 class RegNet(AnyNet):
     """RegNet model."""
 
     @staticmethod
-    def get_args():
+    def get_params():
         """Convert RegNet to AnyNet parameter format."""
-        # Generate RegNet ws per block
+        # Generates per stage ws, ds, gs, bs, and ss from RegNet parameters
         w_a, w_0, w_m, d = cfg.REGNET.WA, cfg.REGNET.W0, cfg.REGNET.WM, cfg.REGNET.DEPTH
-        ws, num_stages, _, _ = generate_regnet(w_a, w_0, w_m, d)
-        # Convert to per stage format
-        s_ws, s_ds = get_stages_from_blocks(ws, ws)
-        # Use the same gw, bm and ss for each stage
-        s_gs = [cfg.REGNET.GROUP_W for _ in range(num_stages)]
-        s_bs = [cfg.REGNET.BOT_MUL for _ in range(num_stages)]
-        s_ss = [cfg.REGNET.STRIDE for _ in range(num_stages)]
-        # Adjust the compatibility of ws and gws
-        s_ws, s_gs = adjust_ws_gs_comp(s_ws, s_bs, s_gs)
+        ws, ds = generate_regnet(w_a, w_0, w_m, d)[0:2]
+        ss = [cfg.REGNET.STRIDE for _ in ws]
+        bs = [cfg.REGNET.BOT_MUL for _ in ws]
+        gs = [cfg.REGNET.GROUP_W for _ in ws]
+        ws, bs, gs = bk.adjust_block_compatibility(ws, bs, gs)
         # Get AnyNet arguments defining the RegNet
         return {
             "stem_type": cfg.REGNET.STEM_TYPE,
             "stem_w": cfg.REGNET.STEM_W,
             "block_type": cfg.REGNET.BLOCK_TYPE,
-            "ds": s_ds,
-            "ws": s_ws,
-            "ss": s_ss,
-            "bms": s_bs,
-            "gws": s_gs,
-            "se_r": cfg.REGNET.SE_R if cfg.REGNET.SE_ON else None,
-            "nc": cfg.MODEL.NUM_CLASSES,
+            "depths": ds,
+            "widths": ws,
+            "strides": ss,
+            "bot_muls": bs,
+            "group_ws": gs,
+            "se_r": cfg.REGNET.SE_R if cfg.REGNET.SE_ON else 0,
+            "num_classes": cfg.MODEL.NUM_CLASSES,
         }
 
     def __init__(self):
-        kwargs = RegNet.get_args()
-        super(RegNet, self).__init__(**kwargs)
+        params = RegNet.get_params()
+        super(RegNet, self).__init__(params)
 
     @staticmethod
-    def complexity(cx, **kwargs):
-        """Computes model complexity. If you alter the model, make sure to update."""
-        kwargs = RegNet.get_args() if not kwargs else kwargs
-        return AnyNet.complexity(cx, **kwargs)
+    def complexity(cx, params=None):
+        """Computes model complexity (if you alter the model, make sure to update)."""
+        params = RegNet.get_params() if not params else params
+        return AnyNet.complexity(cx, params)
--- pycls/models/resnet.py
+++ pycls/models/resnet.py
@@ -7,9 +7,22 @@
 
 """ResNe(X)t models."""
 
-import pycls.core.net as net
-import torch.nn as nn
 from pycls.core.config import cfg
+from pycls.models.blocks import (
+    activation,
+    conv2d,
+    conv2d_cx,
+    gap2d,
+    gap2d_cx,
+    init_weights,
+    linear,
+    linear_cx,
+    norm2d,
+    norm2d_cx,
+    pool2d,
+    pool2d_cx,
+)
+from torch.nn import Module
 
 
 # Stage depths for ImageNet models
@@ -27,13 +40,13 @@ def get_trans_fun(name):
     return trans_funs[name]
 
 
-class ResHead(nn.Module):
+class ResHead(Module):
     """ResNet head: AvgPool, 1x1."""
 
-    def __init__(self, w_in, nc):
+    def __init__(self, w_in, num_classes):
         super(ResHead, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(w_in, nc, bias=True)
+        self.avg_pool = gap2d(w_in)
+        self.fc = linear(w_in, num_classes, bias=True)
 
     def forward(self, x):
         x = self.avg_pool(x)
@@ -42,24 +55,24 @@ class ResHead(nn.Module):
         return x
 
     @staticmethod
-    def complexity(cx, w_in, nc):
-        cx["h"], cx["w"] = 1, 1
-        cx = net.complexity_conv2d(cx, w_in, nc, 1, 1, 0, bias=True)
+    def complexity(cx, w_in, num_classes):
+        cx = gap2d_cx(cx, w_in)
+        cx = linear_cx(cx, w_in, num_classes, bias=True)
         return cx
 
 
-class BasicTransform(nn.Module):
-    """Basic transformation: 3x3, BN, ReLU, 3x3, BN."""
+class BasicTransform(Module):
+    """Basic transformation: 3x3, BN, AF, 3x3, BN."""
 
-    def __init__(self, w_in, w_out, stride, w_b=None, num_gs=1):
-        err_str = "Basic transform does not support w_b and num_gs options"
-        assert w_b is None and num_gs == 1, err_str
+    def __init__(self, w_in, w_out, stride, w_b=None, groups=1):
+        err_str = "Basic transform does not support w_b and groups options"
+        assert w_b is None and groups == 1, err_str
         super(BasicTransform, self).__init__()
-        self.a = nn.Conv2d(w_in, w_out, 3, stride=stride, padding=1, bias=False)
-        self.a_bn = nn.BatchNorm2d(w_out, eps=cfg.BN.EPS, momentum=cfg.BN.MOM)
-        self.a_relu = nn.ReLU(inplace=cfg.MEM.RELU_INPLACE)
-        self.b = nn.Conv2d(w_out, w_out, 3, stride=1, padding=1, bias=False)
-        self.b_bn = nn.BatchNorm2d(w_out, eps=cfg.BN.EPS, momentum=cfg.BN.MOM)
+        self.a = conv2d(w_in, w_out, 3, stride=stride)
+        self.a_bn = norm2d(w_out)
+        self.a_af = activation()
+        self.b = conv2d(w_out, w_out, 3)
+        self.b_bn = norm2d(w_out)
         self.b_bn.final_bn = True
 
     def forward(self, x):
@@ -68,31 +81,31 @@ class BasicTransform(nn.Module):
         return x
 
     @staticmethod
-    def complexity(cx, w_in, w_out, stride, w_b=None, num_gs=1):
-        err_str = "Basic transform does not support w_b and num_gs options"
-        assert w_b is None and num_gs == 1, err_str
-        cx = net.complexity_conv2d(cx, w_in, w_out, 3, stride, 1)
-        cx = net.complexity_batchnorm2d(cx, w_out)
-        cx = net.complexity_conv2d(cx, w_out, w_out, 3, 1, 1)
-        cx = net.complexity_batchnorm2d(cx, w_out)
+    def complexity(cx, w_in, w_out, stride, w_b=None, groups=1):
+        err_str = "Basic transform does not support w_b and groups options"
+        assert w_b is None and groups == 1, err_str
+        cx = conv2d_cx(cx, w_in, w_out, 3, stride=stride)
+        cx = norm2d_cx(cx, w_out)
+        cx = conv2d_cx(cx, w_out, w_out, 3)
+        cx = norm2d_cx(cx, w_out)
         return cx
 
 
-class BottleneckTransform(nn.Module):
-    """Bottleneck transformation: 1x1, BN, ReLU, 3x3, BN, ReLU, 1x1, BN."""
+class BottleneckTransform(Module):
+    """Bottleneck transformation: 1x1, BN, AF, 3x3, BN, AF, 1x1, BN."""
 
-    def __init__(self, w_in, w_out, stride, w_b, num_gs):
+    def __init__(self, w_in, w_out, stride, w_b, groups):
         super(BottleneckTransform, self).__init__()
         # MSRA -> stride=2 is on 1x1; TH/C2 -> stride=2 is on 3x3
         (s1, s3) = (stride, 1) if cfg.RESNET.STRIDE_1X1 else (1, stride)
-        self.a = nn.Conv2d(w_in, w_b, 1, stride=s1, padding=0, bias=False)
-        self.a_bn = nn.BatchNorm2d(w_b, eps=cfg.BN.EPS, momentum=cfg.BN.MOM)
-        self.a_relu = nn.ReLU(inplace=cfg.MEM.RELU_INPLACE)
-        self.b = nn.Conv2d(w_b, w_b, 3, stride=s3, padding=1, groups=num_gs, bias=False)
-        self.b_bn = nn.BatchNorm2d(w_b, eps=cfg.BN.EPS, momentum=cfg.BN.MOM)
-        self.b_relu = nn.ReLU(inplace=cfg.MEM.RELU_INPLACE)
-        self.c = nn.Conv2d(w_b, w_out, 1, stride=1, padding=0, bias=False)
-        self.c_bn = nn.BatchNorm2d(w_out, eps=cfg.BN.EPS, momentum=cfg.BN.MOM)
+        self.a = conv2d(w_in, w_b, 1, stride=s1)
+        self.a_bn = norm2d(w_b)
+        self.a_af = activation()
+        self.b = conv2d(w_b, w_b, 3, stride=s3, groups=groups)
+        self.b_bn = norm2d(w_b)
+        self.b_af = activation()
+        self.c = conv2d(w_b, w_out, 1)
+        self.c_bn = norm2d(w_out)
         self.c_bn.final_bn = True
 
     def forward(self, x):
@@ -101,60 +114,54 @@ class BottleneckTransform(nn.Module):
         return x
 
     @staticmethod
-    def complexity(cx, w_in, w_out, stride, w_b, num_gs):
+    def complexity(cx, w_in, w_out, stride, w_b, groups):
         (s1, s3) = (stride, 1) if cfg.RESNET.STRIDE_1X1 else (1, stride)
-        cx = net.complexity_conv2d(cx, w_in, w_b, 1, s1, 0)
-        cx = net.complexity_batchnorm2d(cx, w_b)
-        cx = net.complexity_conv2d(cx, w_b, w_b, 3, s3, 1, num_gs)
-        cx = net.complexity_batchnorm2d(cx, w_b)
-        cx = net.complexity_conv2d(cx, w_b, w_out, 1, 1, 0)
-        cx = net.complexity_batchnorm2d(cx, w_out)
+        cx = conv2d_cx(cx, w_in, w_b, 1, stride=s1)
+        cx = norm2d_cx(cx, w_b)
+        cx = conv2d_cx(cx, w_b, w_b, 3, stride=s3, groups=groups)
+        cx = norm2d_cx(cx, w_b)
+        cx = conv2d_cx(cx, w_b, w_out, 1)
+        cx = norm2d_cx(cx, w_out)
         return cx
 
 
-class ResBlock(nn.Module):
-    """Residual block: x + F(x)."""
+class ResBlock(Module):
+    """Residual block: x + f(x)."""
 
-    def __init__(self, w_in, w_out, stride, trans_fun, w_b=None, num_gs=1):
+    def __init__(self, w_in, w_out, stride, trans_fun, w_b=None, groups=1):
         super(ResBlock, self).__init__()
-        # Use skip connection with projection if shape changes
-        self.proj_block = (w_in != w_out) or (stride != 1)
-        if self.proj_block:
-            self.proj = nn.Conv2d(w_in, w_out, 1, stride=stride, padding=0, bias=False)
-            self.bn = nn.BatchNorm2d(w_out, eps=cfg.BN.EPS, momentum=cfg.BN.MOM)
-        self.f = trans_fun(w_in, w_out, stride, w_b, num_gs)
-        self.relu = nn.ReLU(cfg.MEM.RELU_INPLACE)
+        self.proj, self.bn = None, None
+        if (w_in != w_out) or (stride != 1):
+            self.proj = conv2d(w_in, w_out, 1, stride=stride)
+            self.bn = norm2d(w_out)
+        self.f = trans_fun(w_in, w_out, stride, w_b, groups)
+        self.af = activation()
 
     def forward(self, x):
-        if self.proj_block:
-            x = self.bn(self.proj(x)) + self.f(x)
-        else:
-            x = x + self.f(x)
-        x = self.relu(x)
-        return x
+        x_p = self.bn(self.proj(x)) if self.proj else x
+        return self.af(x_p + self.f(x))
 
     @staticmethod
-    def complexity(cx, w_in, w_out, stride, trans_fun, w_b, num_gs):
-        proj_block = (w_in != w_out) or (stride != 1)
-        if proj_block:
+    def complexity(cx, w_in, w_out, stride, trans_fun, w_b, groups):
+        if (w_in != w_out) or (stride != 1):
             h, w = cx["h"], cx["w"]
-            cx = net.complexity_conv2d(cx, w_in, w_out, 1, stride, 0)
-            cx = net.complexity_batchnorm2d(cx, w_out)
-            cx["h"], cx["w"] = h, w  # parallel branch
-        cx = trans_fun.complexity(cx, w_in, w_out, stride, w_b, num_gs)
+            cx = conv2d_cx(cx, w_in, w_out, 1, stride=stride)
+            cx = norm2d_cx(cx, w_out)
+            cx["h"], cx["w"] = h, w
+        cx = trans_fun.complexity(cx, w_in, w_out, stride, w_b, groups)
         return cx
 
 
-class ResStage(nn.Module):
+class ResStage(Module):
     """Stage of ResNet."""
 
-    def __init__(self, w_in, w_out, stride, d, w_b=None, num_gs=1):
+    def __init__(self, w_in, w_out, stride, d, w_b=None, groups=1):
         super(ResStage, self).__init__()
         for i in range(d):
             b_stride = stride if i == 0 else 1
             b_w_in = w_in if i == 0 else w_out
             trans_fun = get_trans_fun(cfg.RESNET.TRANS_FUN)
-            res_block = ResBlock(b_w_in, w_out, b_stride, trans_fun, w_b, num_gs)
+            res_block = ResBlock(b_w_in, w_out, b_stride, trans_fun, w_b, groups)
             self.add_module("b{}".format(i + 1), res_block)
 
     def forward(self, x):
@@ -163,23 +170,23 @@ class ResStage(nn.Module):
         return x
 
     @staticmethod
-    def complexity(cx, w_in, w_out, stride, d, w_b=None, num_gs=1):
+    def complexity(cx, w_in, w_out, stride, d, w_b=None, groups=1):
         for i in range(d):
             b_stride = stride if i == 0 else 1
             b_w_in = w_in if i == 0 else w_out
             trans_f = get_trans_fun(cfg.RESNET.TRANS_FUN)
-            cx = ResBlock.complexity(cx, b_w_in, w_out, b_stride, trans_f, w_b, num_gs)
+            cx = ResBlock.complexity(cx, b_w_in, w_out, b_stride, trans_f, w_b, groups)
         return cx
 
 
-class ResStemCifar(nn.Module):
-    """ResNet stem for CIFAR: 3x3, BN, ReLU."""
+class ResStemCifar(Module):
+    """ResNet stem for CIFAR: 3x3, BN, AF."""
 
     def __init__(self, w_in, w_out):
         super(ResStemCifar, self).__init__()
-        self.conv = nn.Conv2d(w_in, w_out, 3, stride=1, padding=1, bias=False)
-        self.bn = nn.BatchNorm2d(w_out, eps=cfg.BN.EPS, momentum=cfg.BN.MOM)
-        self.relu = nn.ReLU(cfg.MEM.RELU_INPLACE)
+        self.conv = conv2d(w_in, w_out, 3)
+        self.bn = norm2d(w_out)
+        self.af = activation()
 
     def forward(self, x):
         for layer in self.children():
@@ -188,20 +195,20 @@ class ResStemCifar(nn.Module):
 
     @staticmethod
     def complexity(cx, w_in, w_out):
-        cx = net.complexity_conv2d(cx, w_in, w_out, 3, 1, 1)
-        cx = net.complexity_batchnorm2d(cx, w_out)
+        cx = conv2d_cx(cx, w_in, w_out, 3)
+        cx = norm2d_cx(cx, w_out)
         return cx
 
 
-class ResStemIN(nn.Module):
-    """ResNet stem for ImageNet: 7x7, BN, ReLU, MaxPool."""
+class ResStemIN(Module):
+    """ResNet stem for ImageNet: 7x7, BN, AF, MaxPool."""
 
     def __init__(self, w_in, w_out):
         super(ResStemIN, self).__init__()
-        self.conv = nn.Conv2d(w_in, w_out, 7, stride=2, padding=3, bias=False)
-        self.bn = nn.BatchNorm2d(w_out, eps=cfg.BN.EPS, momentum=cfg.BN.MOM)
-        self.relu = nn.ReLU(cfg.MEM.RELU_INPLACE)
-        self.pool = nn.MaxPool2d(3, stride=2, padding=1)
+        self.conv = conv2d(w_in, w_out, 7, stride=2)
+        self.bn = norm2d(w_out)
+        self.af = activation()
+        self.pool = pool2d(w_out, 3, stride=2)
 
     def forward(self, x):
         for layer in self.children():
@@ -210,13 +217,13 @@ class ResStemIN(nn.Module):
 
     @staticmethod
     def complexity(cx, w_in, w_out):
-        cx = net.complexity_conv2d(cx, w_in, w_out, 7, 2, 3)
-        cx = net.complexity_batchnorm2d(cx, w_out)
-        cx = net.complexity_maxpool2d(cx, w_out, 3, 2, 1)
+        cx = conv2d_cx(cx, w_in, w_out, 7, stride=2)
+        cx = norm2d_cx(cx, w_out)
+        cx = pool2d_cx(cx, w_out, 3, stride=2)
         return cx
 
 
-class ResNet(nn.Module):
+class ResNet(Module):
     """ResNet model."""
 
     def __init__(self):
@@ -229,7 +236,7 @@ class ResNet(nn.Module):
             self._construct_cifar()
         else:
             self._construct_imagenet()
-        self.apply(net.init_weights)
+        self.apply(init_weights)
 
     def _construct_cifar(self):
         err_str = "Model depth should be of the format 6n + 2 for cifar"
@@ -239,18 +246,18 @@ class ResNet(nn.Module):
         self.s1 = ResStage(16, 16, stride=1, d=d)
         self.s2 = ResStage(16, 32, stride=2, d=d)
         self.s3 = ResStage(32, 64, stride=2, d=d)
-        self.head = ResHead(64, nc=cfg.MODEL.NUM_CLASSES)
+        self.head = ResHead(64, cfg.MODEL.NUM_CLASSES)
 
     def _construct_imagenet(self):
         g, gw = cfg.RESNET.NUM_GROUPS, cfg.RESNET.WIDTH_PER_GROUP
         (d1, d2, d3, d4) = _IN_STAGE_DS[cfg.MODEL.DEPTH]
         w_b = gw * g
         self.stem = ResStemIN(3, 64)
-        self.s1 = ResStage(64, 256, stride=1, d=d1, w_b=w_b, num_gs=g)
-        self.s2 = ResStage(256, 512, stride=2, d=d2, w_b=w_b * 2, num_gs=g)
-        self.s3 = ResStage(512, 1024, stride=2, d=d3, w_b=w_b * 4, num_gs=g)
-        self.s4 = ResStage(1024, 2048, stride=2, d=d4, w_b=w_b * 8, num_gs=g)
-        self.head = ResHead(2048, nc=cfg.MODEL.NUM_CLASSES)
+        self.s1 = ResStage(64, 256, stride=1, d=d1, w_b=w_b, groups=g)
+        self.s2 = ResStage(256, 512, stride=2, d=d2, w_b=w_b * 2, groups=g)
+        self.s3 = ResStage(512, 1024, stride=2, d=d3, w_b=w_b * 4, groups=g)
+        self.s4 = ResStage(1024, 2048, stride=2, d=d4, w_b=w_b * 8, groups=g)
+        self.head = ResHead(2048, cfg.MODEL.NUM_CLASSES)
 
     def forward(self, x):
         for module in self.children():
@@ -266,15 +273,15 @@ class ResNet(nn.Module):
             cx = ResStage.complexity(cx, 16, 16, stride=1, d=d)
             cx = ResStage.complexity(cx, 16, 32, stride=2, d=d)
             cx = ResStage.complexity(cx, 32, 64, stride=2, d=d)
-            cx = ResHead.complexity(cx, 64, nc=cfg.MODEL.NUM_CLASSES)
+            cx = ResHead.complexity(cx, 64, cfg.MODEL.NUM_CLASSES)
         else:
             g, gw = cfg.RESNET.NUM_GROUPS, cfg.RESNET.WIDTH_PER_GROUP
             (d1, d2, d3, d4) = _IN_STAGE_DS[cfg.MODEL.DEPTH]
             w_b = gw * g
             cx = ResStemIN.complexity(cx, 3, 64)
-            cx = ResStage.complexity(cx, 64, 256, 1, d=d1, w_b=w_b, num_gs=g)
-            cx = ResStage.complexity(cx, 256, 512, 2, d=d2, w_b=w_b * 2, num_gs=g)
-            cx = ResStage.complexity(cx, 512, 1024, 2, d=d3, w_b=w_b * 4, num_gs=g)
-            cx = ResStage.complexity(cx, 1024, 2048, 2, d=d4, w_b=w_b * 8, num_gs=g)
-            cx = ResHead.complexity(cx, 2048, nc=cfg.MODEL.NUM_CLASSES)
+            cx = ResStage.complexity(cx, 64, 256, 1, d=d1, w_b=w_b, groups=g)
+            cx = ResStage.complexity(cx, 256, 512, 2, d=d2, w_b=w_b * 2, groups=g)
+            cx = ResStage.complexity(cx, 512, 1024, 2, d=d3, w_b=w_b * 4, groups=g)
+            cx = ResStage.complexity(cx, 1024, 2048, 2, d=d4, w_b=w_b * 8, groups=g)
+            cx = ResHead.complexity(cx, 2048, cfg.MODEL.NUM_CLASSES)
         return cx
