4cf2524cdaa87064698427ee071061998cead733
Ilija Radosavovic <ilija.radosavovic@gmail.com>
2020-09-08 17:50:09 -0700

    Fix packages entry in setup.py (#108)
    Summary: Pull Request resolved: https://github.com/facebookresearch/pycls/pull/108
    
    Reviewed By: pdollar
    
    Differential Revision: D23552764
    
    Pulled By: rajprateek
    
    fbshipit-source-id: e43ccd512d7fa0400e13ccaef7b1a5f394d918d7

--- setup.py
+++ setup.py
@@ -7,7 +7,27 @@
 
 """Setup pycls."""
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
-setup(name="pycls", packages=["pycls"])
+def readme():
+    """Retrieves the readme content."""
+    with open("README.md", "r") as f:
+        content = f.read()
+    return content
+
+
+setup(
+    name="pycls",
+    version="0.1.1",
+    description="A codebase for image classification",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/facebookresearch/pycls",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=["numpy", "opencv-python", "simplejson", "yacs"],
+)
