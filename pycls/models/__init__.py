822ae76b82ed1d359d299937127017074a9a1c06
王枫02-BaseDetection <wangfeng02@megvii.com>
2021-04-06 16:30:30 +0800

    fix(models): fix a bug of recursive import

--- pycls/models/__init__.py
+++ pycls/models/__init__.py
@@ -4,7 +4,3 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-
-"""Expose model zoo constructors."""
-
-from pycls.models.model_zoo import effnet, regnetx, regnety, resnet, resnext
