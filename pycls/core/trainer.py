0a97464750e1ae9d0b869e07e2ca17b5278e466d
Vaibhav Aggarwal <vaibhava@fb.com>
2022-03-21 13:08:57 -0700

    Enable FSDP usage for PyCls models (#176)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pycls/pull/176
    
    Add support for running PyCls ViT models with [FSDP](https://fairscale.readthedocs.io/en/latest/api/nn/fsdp.html).
    
    Authors: Anjali Sridhar, Vaibhav Aggarwal
    
    Reviewed By: sdebnathusc
    
    Differential Revision: D34689650
    
    fbshipit-source-id: 062021781d69383fa4f6fe114cf92e30db068c36

--- pycls/core/trainer.py
+++ pycls/core/trainer.py
@@ -7,6 +7,7 @@
 
 """Tools for training and testing a model."""
 
+import contextlib
 import os
 import random
 from copy import deepcopy
@@ -24,11 +25,71 @@ import pycls.core.optimizer as optim
 import pycls.datasets.loader as data_loader
 import torch
 import torch.cuda.amp as amp
+from fairscale.nn import auto_wrap, config_auto_wrap_policy, enable_wrap
+from fairscale.nn.data_parallel import FullyShardedDataParallel as FSDP
+from fairscale.optim.grad_scaler import ShardedGradScaler
 from pycls.core.config import cfg
 from pycls.core.io import cache_url, pathmgr
 
 
 logger = logging.get_logger(__name__)
+
+
+def auto_wrap_ln(module, fsdp_config=None, wrap_it=None, assert_on_collision=None):
+    """
+    Auto wrap all LayerNorm instances with a safer FSDP, esp. when convert
+    to LayerNorm is used and the outer FSDP is flattening.
+    We put KN in is own full precision, unflatten, single GPU group FSDP.  Note, LNs still have
+    a group size == world_size. The input and output for LN are still FP16 in mixed precision mode.
+    See ``keep_batchnorm_fp32`` here: https://nvidia.github.io/apex/amp.html
+    This needs to be done at each rank, like models being wrapped by FSDP at each rank.
+    Args:
+        module (nn.Module):
+            The model (or part of the model) in which BN to be pre-wrapped.
+        process_group (ProcessGroup):
+            Optional process group to be used.
+        fsdp_config (Dict):
+            Optional fsdp_config to be used.
+        wrap_it (bool):
+            Whether or not wrap the module after setting the config.
+            Default: True
+        assert_on_collision (bool):
+            Whether or not assert if a wrapper_config already exists on the module.
+            Default: True
+    Returns:
+        Processed module, where BNs are wrapped with a special FSDP instance.
+    """
+    if fsdp_config is None:
+        fsdp_config = {
+            "mixed_precision": False,  # Keep the weights in FP32.
+            "flatten_parameters": False,  # Do not flatten.
+            # Reshard==False is good for performance. When FSDP(checkpoint(FSDP(bn))) is used, this
+            # **must** be False because BN's FSDP wrapper's pre-backward callback isn't called
+            # within the checkpoint's outer backward when multiple forward passes are used.
+            "reshard_after_forward": False,
+            # No bucketing or small bucketing should be enough for BNs.
+            "bucket_cap_mb": 0,
+            # Setting this for SyncBatchNorm. This may have a performance impact. If
+            # SyncBatchNorm is used, this can be enabled by passing in the `fsdp_config` argument.
+            "force_input_to_fp32": False,
+        }
+
+    # Assign the config dict to BNs.
+    for m in module.modules():
+        if isinstance(m, torch.nn.LayerNorm):
+            if assert_on_collision:
+                assert not hasattr(
+                    m, "wrapper_config"
+                ), "Module shouldn't already have a wrapper_config. Is it tagged already by another policy?"
+            m.wrapper_config = fsdp_config
+
+    # Wrap it.
+    with (
+        enable_wrap(config_auto_wrap_policy, wrapper_cls=FSDP)
+        if wrap_it
+        else contextlib.suppress()
+    ):
+        return auto_wrap(module)
 
 
 def setup_env():
@@ -60,18 +121,48 @@ def setup_model():
     """Sets up a model for training or testing and log the results."""
     # Build the model
     model = builders.build_model()
+
+    if cfg.FSDP.ENABLED:
+        fsdp_config = {}
+        fsdp_config["reshard_after_forward"] = cfg.FSDP.RESHARD_AFTER_FW
+        fsdp_config["mixed_precision"] = cfg.TRAIN.MIXED_PRECISION
+        if cfg.TRAIN.MIXED_PRECISION:
+            fsdp_config["clear_autocast_cache"] = True
+
+        # Enable LAYER_NORM_FP32 wrapping for mixed precision training only. It is not
+        # required for full precision training.
+        ema_model = builders.build_model()
+        if cfg.TRAIN.MIXED_PRECISION and cfg.FSDP.LAYER_NORM_FP32:
+
+            def do_wrap(m):
+                m = auto_wrap_ln(m)
+                return FSDP(m, **fsdp_config)
+
+            model = do_wrap(model)
+            ema_model = do_wrap(ema_model)
+        else:
+            with enable_wrap(wrapper_cls=FSDP, **fsdp_config):
+                model = FSDP(model, **fsdp_config)
+                ema_model = FSDP(ema_model, **fsdp_config)
+
+    else:
+        # Build the model
+        model = builders.build_model()
+        ema_model = deepcopy(model)
+
     logger.info("Model:\n{}".format(model)) if cfg.VERBOSE else ()
     # Log model complexity
     logger.info(logging.dump_log_data(net.complexity(model), "complexity"))
     # Transfer the model to the current GPU device
     cur_device = torch.cuda.current_device()
     model = model.cuda(device=cur_device)
+    ema_model = ema_model.cuda(device=cur_device)
     # Use multi-process data parallel model in the multi-gpu setting
     if cfg.NUM_GPUS > 1:
         # Make model replica operate on the current device
         ddp = torch.nn.parallel.DistributedDataParallel
         model = ddp(module=model, device_ids=[cur_device], output_device=cur_device)
-    return model
+    return model, ema_model
 
 
 def get_weights_file(weights_file):
@@ -161,8 +252,7 @@ def train_model():
     # Setup training/testing environment
     setup_env()
     # Construct the model, ema, loss_fun, and optimizer
-    model = setup_model()
-    ema = deepcopy(model)
+    model, ema = setup_model()
     loss_fun = builders.build_loss_fun().cuda()
     optimizer = optim.construct_optimizer(model)
     # Load checkpoint or initial weights
@@ -183,7 +273,10 @@ def train_model():
     test_meter = meters.TestMeter(len(test_loader))
     ema_meter = meters.TestMeter(len(test_loader), "test_ema")
     # Create a GradScaler for mixed precision training
-    scaler = amp.GradScaler(enabled=cfg.TRAIN.MIXED_PRECISION)
+    if cfg.FSDP.ENABLED:
+        scaler = ShardedGradScaler(enabled=cfg.TRAIN.MIXED_PRECISION)
+    else:
+        scaler = amp.GradScaler(enabled=cfg.TRAIN.MIXED_PRECISION)
     # Compute model and loader timings
     if start_epoch == 0 and cfg.PREC_TIME.NUM_ITER > 0:
         benchmark.compute_time_full(model, loss_fun, train_loader, test_loader)
@@ -212,7 +305,7 @@ def test_model():
     # Setup training/testing environment
     setup_env()
     # Construct the model
-    model = setup_model()
+    model, _ = setup_model()
     # Load model weights
     test_weights = get_weights_file(cfg.TEST.WEIGHTS)
     cp.load_checkpoint(test_weights, model)
@@ -229,7 +322,7 @@ def time_model():
     # Setup training/testing environment
     setup_env()
     # Construct the model and loss_fun
-    model = setup_model()
+    model, _ = setup_model()
     loss_fun = builders.build_loss_fun().cuda()
     # Compute model and loader timings
     benchmark.compute_time_model(model, loss_fun)
@@ -240,7 +333,7 @@ def time_model_and_loader():
     # Setup training/testing environment
     setup_env()
     # Construct the model and loss_fun
-    model = setup_model()
+    model, _ = setup_model()
     loss_fun = builders.build_loss_fun().cuda()
     # Create data loaders
     train_loader = data_loader.construct_train_loader()
