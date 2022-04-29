371222cc30f44259923f25909517e6e0f76e8fce
hejian* <flystarhe@qq.com>
2022-04-29 15:42:30 +0800

    fix get_loss_fun() err message

--- pycls/core/builders.py
+++ pycls/core/builders.py
@@ -40,7 +40,7 @@ def get_model():
 def get_loss_fun():
     """Gets the loss function class specified in the config."""
     err_str = "Loss function type '{}' not supported"
-    assert cfg.MODEL.LOSS_FUN in _loss_funs.keys(), err_str.format(cfg.TRAIN.LOSS)
+    assert cfg.MODEL.LOSS_FUN in _loss_funs.keys(), err_str.format(cfg.MODEL.LOSS_FUN)
     return _loss_funs[cfg.MODEL.LOSS_FUN]
 
 
